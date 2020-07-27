5ced7bcf30946a1d7be2b1ca311f4a6e523ec95a
Jayla Nickens <jaylanickensgill11@gmail.com>
2020-07-27 15:40:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def my_function(fname):
+   print (fname + " ")
+
+my_function(" name is Jayla")  
+my_function(" brithday is August 26")
+my_function(" was born in 2005")
+my_function(" Zodiac sign is a virgo")
+my_function("Chinese sign is a rooster")
+print
